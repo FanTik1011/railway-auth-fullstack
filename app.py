0d803d34fb074da7a92b0d442af6f94992a105a2
FanTik1011 <140693025+FanTik1011@users.noreply.github.com>
2025-05-14 13:02:40 +0300

    2

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from models import db, User
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, Length
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db.init_app(app)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6)])
+    submit = SubmitField('Register')
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful!', 'success')
+        return redirect(url_for('register'))
+    return render_template('register.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
